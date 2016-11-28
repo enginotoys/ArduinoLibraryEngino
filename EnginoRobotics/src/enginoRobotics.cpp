37326bf2f8afb348d15fcb4450514463dd179e9f
Stelios Ioakim <s.ioakim@engino.net>
2016-11-28 10:16:51 +0200

    push

--- EnginoRobotics/src/enginoRobotics.cpp
+++ EnginoRobotics/src/enginoRobotics.cpp
@@ -1,7 +1,25 @@
 #include "enginoRobotics.h"
 
-ERP::ERP()
+void EnginoRobotics::motor(uint8_t port, uint8_t direction, uint8_t speed)
 {
-  
-  
+    Serial.write(0xFF);
+	Serial.write(0xFA);
+	Serial.write(0x08);
+	Serial.write(0x04);
+	Serial.write(port);
+	Serial.write(direction);
+	Serial.write(speed);
+	Serial.write(0xEE);
+}
+
+void EnginoRobotics::setRGB(uint8_t red, uint8_t green, uint8_t blue)
+{
+	Serial.write(0xFF);
+	Serial.write(0xFA);
+	Serial.write(0x08);
+	Serial.write(0x05);
+	Serial.write(red);
+	Serial.write(green);
+	Serial.write(blue);
+	Serial.write(0xEE);
 }
--- EnginoRobotics/src/enginoRobotics.h
+++ EnginoRobotics/src/enginoRobotics.h
@@ -2,11 +2,27 @@
 #define __ENGINOROBOTICS_H__
 
 #include <Arduino.h>
+#include <inttypes.h>
 
 class EnginoRobotics 
 {
   public:
-    ERP();  
+    void motor(uint8_t port, uint8_t direction, uint8_t speed);
+    void setRGB(uint8_t red, uint8_t green, uint8_t blue);  
 };
 
 #endif
+
+/*
+
+
+	RX_CMD_SET_LED,
+	RX_CMD_SET_LED_PWM,
+	RX_CMD_SET_SERVO,
+	RX_CMD_GET_TOUCH,
+	RX_CMD_GET_IR,
+	RX_CMD_CONFIG_PORT_SERVO180,
+	RX_CMD_CONFIG_PORT_LED_PWM,
+	RX_CMD_CONFIG_PORT,
+	RX_CMD_START_IR_ENGINE,
+	RX_CMD_ACTIVE_PERIPHERAL_INIT,*/
