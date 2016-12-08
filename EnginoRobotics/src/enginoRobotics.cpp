e280db32ef648a6b5a78b23827c45ba51cecaabe
Stelios Ioakim <s.ioakim@engino.net>
2016-12-08 17:11:03 +0200

    spi switch still testing and some more functions to convert

--- EnginoRobotics/src/enginoRobotics.cpp
+++ EnginoRobotics/src/enginoRobotics.cpp
@@ -1,290 +1,337 @@
 #include "enginoRobotics.h"
+#include "SPI.h"
 
-void EnginoRobotics::getERPType()
+void EnginoRobotics::Begin()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x01);
-	Serial.write(0xEE);
+	SPI.begin();
+  	pinMode(CS, OUTPUT);
+  	digitalWrite(CS, HIGH);
+
+  	delay(1);
+}
+
+void EnginoRobotics::sendCMD(cmd_t spi_cmd)
+{
+	SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+	digitalWrite(CS, LOW);
+
+	SPI.transfer(spi_cmd);
+
+	digitalWrite(CS, HIGH);
+	SPI.endTransaction();
+
+	delayMicroseconds(SPI_DELAY);
+}
+
+void EnginoRobotics::sendBuff(uint8_t * packet, uint8_t len)
+{
+	SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+	digitalWrite(CS, LOW);
+
+	SPI.transfer(packet, len);
+
+	digitalWrite(CS, HIGH);
+	SPI.endTransaction();
+
+	delayMicroseconds(SPI_DELAY);
+}
+
+uint8_t EnginoRobotics::getByteSPI()
+{
+	uint8_t temp;
+
+	SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+	digitalWrite(CS, LOW);
+
+	temp = SPI.transfer(0xFF);
+
+	digitalWrite(CS, HIGH);
+	SPI.endTransaction();
+
+	delayMicroseconds(SPI_DELAY);
+
+	return temp;
+}
+
+void EnginoRobotics::getBufferSPI(uint8_t * dataBuf, uint8_t len)
+{
+	SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+	digitalWrite(CS, LOW);
+
+	SPI.transfer(dataBuf, len);
+
+	digitalWrite(CS, HIGH);
+	SPI.endTransaction();
+
+	delayMicroseconds(SPI_DELAY);
+}
+
+char * EnginoRobotics::getERPType()
+{
+	uint8_t buffer[10] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_ERP_TYPE);
+
+	getBufferSPI(buffer, 10);
+
+	return buffer;
 } 
 
-void EnginoRobotics::getHWVersion()
+char * EnginoRobotics::getHWVersion()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x02);
-	Serial.write(0xEE);
+	uint8_t buffer[7] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_HW_VERSION);
+
+	getBufferSPI(buffer, 7);
+
+	return buffer;
 } 
 
-void EnginoRobotics::getFWVersion()
+char * EnginoRobotics::getFWVersion()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x03);
-	Serial.write(0xEE);
+	uint8_t buffer[6] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_FW_VERSION);
+
+	getBufferSPI(buffer, 6);
+
+	return buffer;
 } 
 
 void EnginoRobotics::setMotor(uint8_t port, uint8_t direction, uint8_t speed)
 {
-    Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x08); //length
-	Serial.write(0x04);
-	Serial.write(port);
-	Serial.write(direction);
-	Serial.write(speed);
-	Serial.write(0xEE);
+    uint8_t buffer[4] = {RX_CMD_SET_MOTOR, port, direction, speed};
+
+   	sendBuff(buffer, 4);
 }
 
 void EnginoRobotics::setRGB(uint8_t red, uint8_t green, uint8_t blue)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x08);
-	Serial.write(0x05);
-	Serial.write(red);
-	Serial.write(green);
-	Serial.write(blue);
-	Serial.write(0xEE);
+	uint8_t buffer[4] = {RX_CMD_SET_RGB, red, green, blue};
+
+   	sendBuff(buffer, 4);
 }
 
 void EnginoRobotics::setLed(uint8_t port, uint8_t state)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x07);
-	Serial.write(0x06);
-	Serial.write(port);
-	Serial.write(state);
-	Serial.write(0xEE);
+	uint8_t buffer[3] = {RX_CMD_SET_LED, port, state};
+
+   	sendBuff(buffer, 3);
 }
 
 void EnginoRobotics::setLedPWM(uint8_t channel, uint8_t duty)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x07);
-	Serial.write(0x07);
-	Serial.write(channel);
-	Serial.write(duty);
-	Serial.write(0xEE);
+	uint8_t buffer[3] = {RX_CMD_SET_LED_PWM, channel, duty};
+
+   	sendBuff(buffer, 3);
 }
 
 void EnginoRobotics::setServo(uint8_t channel, uint8_t angle)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x07);
-	Serial.write(0x08);
-	Serial.write(channel);
-	Serial.write(angle);
-	Serial.write(0xEE);
+	uint8_t buffer[3] = {RX_CMD_SET_SERVO, channel, angle};
+
+   	sendBuff(buffer, 3);
 }
 
 bool EnginoRobotics::getTouch(uint8_t port)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x06);
-	Serial.write(0x09);
-	Serial.write(port);
-	Serial.write(0xEE);
-	
-	while(Serial.available() == 0);
-	
-	return Serial.read();
+	uint8_t buffer[2] = {RX_CMD_GET_TOUCH, port};
+
+   	sendBuff(buffer, 2);
+
+   	return getByteSPI();
 }
 
 bool EnginoRobotics::getIR(uint8_t port)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x06);
-	Serial.write(0x0A);
-	Serial.write(port);
-	Serial.write(0xEE);
-	
-	while(Serial.available() == 0);
-	
-	return Serial.read();
+	uint8_t buffer[2] = {RX_CMD_GET_IR, port};
+
+   	sendBuff(buffer, 2);
+
+   	return getByteSPI();
 }
 
 
-void EnginoRobotics::getColour()
+void EnginoRobotics::getColour(uint16_t *r, uint16_t *g, uint16_t *b, uint16_t *c)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x0B);
-	Serial.write(0xEE);
+	uint8_t buffer[8] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_COLOUR);
+
+	getBufferSPI(buffer, 8);
+
+ 	*r = ((buffer[1] << 8) | (buffer[0]));
+  	*g = ((buffer[3] << 8) | (buffer[2]));
+  	*b = ((buffer[5] << 8) | (buffer[4]));
+ 	*c = ((buffer[7] << 8) | (buffer[6]));
 } 
 
 uint16_t EnginoRobotics::getColourRed()
 {
-	uint16_t val;
+	uint8_t buffer[2] = {0xFF,0xFF};
 
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x0C);
-	Serial.write(0xEE);
+	sendCMD(RX_CMD_GET_COLOUR_RED);
 
-	while(Serial.available() != 2);
+	getBufferSPI(buffer, 2);
 
-	val = Serial.read();
-	val |= (Serial.read() << 8);
-
-	return val;
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 
 uint16_t EnginoRobotics::getColourGreen()
 {
-	uint16_t val;
+	uint8_t buffer[2] = {0xFF,0xFF};
 
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x0D);
-	Serial.write(0xEE);
+	sendCMD(RX_CMD_GET_COLOUR_GREEN);
 
-	while(Serial.available() != 2);
-	
-	val = Serial.read();
-	val |= (Serial.read() << 8);
+	getBufferSPI(buffer, 2);
 
-	return val;
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 uint16_t EnginoRobotics::getColourBlue()
 {
-	uint16_t val;
+	uint8_t buffer[2] = {0xFF,0xFF};
 
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x0E);
-	Serial.write(0xEE);
+	sendCMD(RX_CMD_GET_COLOUR_BLUE);
 
-	while(Serial.available() != 2);
-	
-	val = Serial.read();
-	val |= (Serial.read() << 8);
+	getBufferSPI(buffer, 2);
 
-	return val;
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 
 uint16_t EnginoRobotics::getColourClear()
 {
-	uint16_t val;
+	uint8_t buffer[2] = {0xFF,0xFF};
 
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x0F);
-	Serial.write(0xEE);
+	sendCMD(RX_CMD_GET_COLOUR_CLEAR);
 
-	while(Serial.available() != 2);
-	
-	val = Serial.read();
-	val |= (Serial.read() << 8);
+	getBufferSPI(buffer, 2);
 
-	return val;
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 
-void EnginoRobotics::getAcc()
+void EnginoRobotics::getAcc(int16_t* x, int16_t* y, int16_t* z)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x10);
-	Serial.write(0xEE);
+	uint8_t buffer[6] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_ACC);
+
+	getBufferSPI(buffer, 8);
+
+ 	*x = ((buffer[1] << 8) | (buffer[0]));
+  	*y = ((buffer[3] << 8) | (buffer[2]));
+  	*z = ((buffer[5] << 8) | (buffer[4]));
 } 
 
-void EnginoRobotics::getAccX()
+int16_t EnginoRobotics::getAccX()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x11);
-	Serial.write(0xEE);
+	uint8_t buffer[2] = {0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_ACC_X);
+
+	getBufferSPI(buffer, 2);
+
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 
-void EnginoRobotics::getAccY()
+int16_t EnginoRobotics::getAccY()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x12);
-	Serial.write(0xEE);
+	uint8_t buffer[2] = {0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_ACC_Y);
+
+	getBufferSPI(buffer, 2);
+
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 
-void EnginoRobotics::getAccZ()
+int16_t EnginoRobotics::getAccZ()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x13);
-	Serial.write(0xEE);
+	uint8_t buffer[2] = {0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_ACC_Z);
+
+	getBufferSPI(buffer, 2);
+
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 
-void EnginoRobotics::getGyro()
+void EnginoRobotics::getGyro(int16_t* x, int16_t* y, int16_t* z)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x14);
-	Serial.write(0xEE);
+	uint8_t buffer[6] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_GYRO);
+
+	getBufferSPI(buffer, 8);
+
+ 	*x = ((buffer[1] << 8) | (buffer[0]));
+  	*y = ((buffer[3] << 8) | (buffer[2]));
+  	*z = ((buffer[5] << 8) | (buffer[4]));
 } 
 
-void EnginoRobotics::getGyroX()
+int16_t EnginoRobotics::getGyroX()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x15);
-	Serial.write(0xEE);
-} 
+	uint8_t buffer[2] = {0xFF,0xFF};
 
+	sendCMD(RX_CMD_GET_GYRO_X);
 
-void EnginoRobotics::getGyroY()
-{
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x16);
-	Serial.write(0xEE);
-} 
+	getBufferSPI(buffer, 2);
 
-
-void EnginoRobotics::getGyroZ()
-{
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x17);
-	Serial.write(0xEE);
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 
 
-void EnginoRobotics::getMPU6050Temp()
+int16_t EnginoRobotics::getGyroY()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x18);
-	Serial.write(0xEE);
+	uint8_t buffer[2] = {0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_GYRO_Y);
+
+	getBufferSPI(buffer, 2);
+
+	return ((buffer[1] << 8) | (buffer[0]));
 } 
 
 
-void EnginoRobotics::getMPU6050()
+int16_t EnginoRobotics::getGyroZ()
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x19);
-	Serial.write(0xEE);
+	uint8_t buffer[2] = {0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_GYRO_Z);
+
+	getBufferSPI(buffer, 2);
+
+	return ((buffer[1] << 8) | (buffer[0]));
+} 
+
+
+int16_t EnginoRobotics::getMPU6050Temp()
+{
+	uint8_t buffer[2] = {0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_MPU6050_TEMP);
+
+	getBufferSPI(buffer, 2);
+
+	return ((buffer[1] << 8) | (buffer[0]));
+} 
+
+
+void EnginoRobotics::getMPU6050(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz, int16_t* temp)
+{
+	uint8_t buffer[14] = {0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
+
+	sendCMD(RX_CMD_GET_MPU6050);
+
+	getBufferSPI(buffer, 8);
+
+ 	*ax = ((buffer[1] << 8) | (buffer[0]));
+  	*ay = ((buffer[3] << 8) | (buffer[2]));
+  	*az = ((buffer[5] << 8) | (buffer[4]));
+  	*gx = ((buffer[7] << 8) | (buffer[6]));
+  	*gy = ((buffer[9] << 8) | (buffer[8]));
+  	*gz = ((buffer[11] << 8) | (buffer[10]));
+  	*temp = ((buffer[13] << 8) | (buffer[12]));
 } 
 
 
@@ -300,20 +347,13 @@ void EnginoRobotics::getNRF52Temp()
 
 uint16_t EnginoRobotics::getUltrasonic()
 {
-	uint16_t val;
+	uint8_t buffer[2] = {0xFF,0xFF};
 
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x05);
-	Serial.write(0x1B);
-	Serial.write(0xEE);
+	sendCMD(RX_CMD_GET_ULTRASONIC);
 
-	while(Serial.available() != 2);
-	
-	val = Serial.read();
-	val |= (Serial.read() << 8);
+	getBufferSPI(buffer, 2);
 
-	return val;
+	return ((buffer[1] << 8) | (buffer[0]));
 }
 
 void EnginoRobotics::configPortServo(uint8_t portA,uint8_t portB,uint8_t portC, uint8_t portD)
@@ -331,26 +371,17 @@ void EnginoRobotics::configPortServo(uint8_t portA,uint8_t portB,uint8_t portC, 
 
 void EnginoRobotics::configPortLedPWM(uint8_t portA,uint8_t portB,uint8_t portC, uint8_t portD)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x09);
-	Serial.write(0x1D);
-	Serial.write(portA);
-	Serial.write(portB);
-	Serial.write(portC);
-	Serial.write(portD);
-	Serial.write(0xEE);
+	//need to give a bit more flexibility here.. allow one port setting at a time etc
+	uint8_t buffer[5] = {RX_CMD_CONFIG_PORT_LED_PWM, portA, portB, portC, portD};
+
+   	sendBuff(buffer, 5);
 }
 
-void EnginoRobotics::configPort(uint8_t port, uint8_t element,uint8_t state){
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x08);
-	Serial.write(0x1E);
-	Serial.write(port);
-	Serial.write(element);
-	Serial.write(state);
-	Serial.write(0xEE);
+void EnginoRobotics::configPort(uint8_t port, uint8_t element,uint8_t state)
+{
+	uint8_t buffer[4] = {RX_CMD_CONFIG_PORT, port, element, state};
+
+   	sendBuff(buffer, 4);
 }
 
 void EnginoRobotics::condigLineIRThreshold()
@@ -373,18 +404,13 @@ void EnginoRobotics::condigObstacleIRThreshold()
 }
 
 
-uint8_t EnginoRobotics::calibrateIRThreshold(uint8_t port)
+bool EnginoRobotics::calibrateIRThreshold(uint8_t port)
 {
-	Serial.write(0xFF);
-	Serial.write(0xFA);
-	Serial.write(0x06);
-	Serial.write(0x21);
-	Serial.write(port);
-	Serial.write(0xEE);
+	uint8_t buffer[2] = {RX_CMD_CALIBRATE_THRESHOLD, port};
 
-	while(Serial.available() == 0);
-	
-	return Serial.read();
+   	sendBuff(buffer, 2);
+
+   	return getByteSPI();
 }
 
 
--- EnginoRobotics/src/enginoRobotics.h
+++ EnginoRobotics/src/enginoRobotics.h
@@ -4,6 +4,14 @@
 #include <Arduino.h>
 #include <inttypes.h>
 
+#define SPI_DELAY			50
+#define CS 					9
+
+#define CLOCKWISE			0x10
+#define ANTICLOCKWISE		0x20
+#define BRAKE				0x30
+#define OFF					0x40
+
 typedef enum portEnums
 {
 	MOTORA = 0,
@@ -27,12 +35,59 @@ enum IO_types {
 	MPU6050
 };
 
+typedef enum CMD_LIST
+{
+	RX_CMD_NULL,		
+	RX_CMD_GET_ERP_TYPE, //checked
+	RX_CMD_GET_HW_VERSION,//checked
+	RX_CMD_GET_FW_VERSION,//checked
+	RX_CMD_SET_MOTOR,//checked
+	RX_CMD_SET_RGB,//checked
+	RX_CMD_SET_LED,//checked
+	RX_CMD_SET_LED_PWM,//checked
+	RX_CMD_SET_SERVO,
+	RX_CMD_GET_TOUCH, //checked
+	RX_CMD_GET_IR,
+	RX_CMD_GET_COLOUR,
+	RX_CMD_GET_COLOUR_RED,
+	RX_CMD_GET_COLOUR_GREEN,
+	RX_CMD_GET_COLOUR_BLUE,
+	RX_CMD_GET_COLOUR_CLEAR,
+	RX_CMD_GET_ACC,
+	RX_CMD_GET_ACC_X,
+	RX_CMD_GET_ACC_Y,
+	RX_CMD_GET_ACC_Z,
+	RX_CMD_GET_GYRO,
+	RX_CMD_GET_GYRO_X,
+	RX_CMD_GET_GYRO_Y,
+	RX_CMD_GET_GYRO_Z,
+	RX_CMD_GET_MPU6050_TEMP,
+	RX_CMD_GET_MPU6050,
+	RX_CMD_GET_NRF52_TEMP,
+	RX_CMD_GET_ULTRASONIC, //checked
+	RX_CMD_CONFIG_PORT_SERVO180,
+	RX_CMD_CONFIG_PORT_LED_PWM, //checked
+	RX_CMD_CONFIG_PORT,
+	RX_CMD_CONFIG_LINE_IR_THRESHOLD,
+	RX_CMD_CONFIG_OBSTACLE_IR_THRESHOLD,
+	RX_CMD_CALIBRATE_THRESHOLD,
+	RX_CMD_START_IR_ENGINE,
+	RX_CMD_ACTIVE_PERIPHERAL_INIT
+}cmd_t;
+
 class EnginoRobotics 
 {
-  public:
-	void getERPType();
-	void getHWVersion();
-	void getFWVersion();
+private:
+	void sendCMD(cmd_t spi_cmd);
+	void getBufferSPI(uint8_t * dataBuf, uint8_t len);
+	uint8_t getByteSPI();
+	void sendBuff(uint8_t * packet, uint8_t len);
+
+public:
+  	void Begin();
+	char * getERPType();
+	char * getHWVersion();
+	char * getFWVersion();
     void setMotor(uint8_t port, uint8_t direction, uint8_t speed);
     void setRGB(uint8_t red, uint8_t green, uint8_t blue);  
 	void setLed(uint8_t port, uint8_t state);
@@ -40,21 +95,21 @@ class EnginoRobotics
 	void setServo(uint8_t channel, uint8_t angle);
     bool getTouch(uint8_t port);  
 	bool getIR(uint8_t port);
-	void getColour();
+	void EnginoRobotics::getColour(uint16_t *r, uint16_t *g, uint16_t *b, uint16_t *c);
 	uint16_t getColourRed();
 	uint16_t getColourGreen();
 	uint16_t getColourBlue();
 	uint16_t getColourClear();
-	void getAcc();
-	void getAccX();
-	void getAccY();
-	void getAccZ();
-	void getGyro();
-	void getGyroX();
-	void getGyroY();
-	void getGyroZ();
-	void getMPU6050Temp();
-	void getMPU6050();
+	void getAcc(int16_t* x, int16_t* y, int16_t* z);
+	int16_t getAccX();
+	int16_t getAccY();
+	int16_t getAccZ();
+	void getGyro(int16_t* x, int16_t* y, int16_t* z);
+	int16_t getGyroX();
+	int16_t getGyroY();
+	int16_t getGyroZ();
+	int16_t getMPU6050Temp();
+	void getMPU6050(int16_t* ax, int16_t* ay, int16_t* az, int16_t* gx, int16_t* gy, int16_t* gz, int16_t* temp);
 	void getNRF52Temp();
 	uint16_t getUltrasonic();
 	void configPortServo(uint8_t portA,uint8_t portB,uint8_t portC, uint8_t portD);
@@ -62,7 +117,7 @@ class EnginoRobotics
 	void configPort(uint8_t port,uint8_t element,uint8_t state);
 	void condigLineIRThreshold();
 	void condigObstacleIRThreshold();
-	uint8_t calibrateIRThreshold(uint8_t port);
+	bool calibrateIRThreshold(uint8_t port);
 	void StartIREnigine();
 	void ActivePeripheralInit();
 };
